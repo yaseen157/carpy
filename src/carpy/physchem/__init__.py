78da0949083c9f6d0b3d88a1dad4846482b22b45
Yaseen Reza <57412770+yaseen157@users.noreply.github.com>
2024-08-18 17:20:46 +0100

    recovery part 2

--- src/carpy/physchem/__init__.py
+++
@@ -1,6 +0,0 @@
-"""A package for modelling the physical chemistry (physchem) of fluids for engineering purposes."""
-from ._atom import *
-from ._chemical_species import *
-from ._chemical_structure import *
-from ._equations_of_state import *
-from ._fluid_models import *
