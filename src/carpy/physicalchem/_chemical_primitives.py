Updated to NumPy 2.0, fixed issue in ansatz bond force constant not having default units

src/carpy/physicalchem/_chemical_primitives.py
@@ -513,10 +513,12 @@ class CovalentBond:
                 _k = ansatz = sum(arr := [x for sublist in list(bond_data.values()) for x in sublist]) / len(arr)
 
             if np.isnan(_k):
-                warn_msg = f"Could not find force constant data for the {type(self).__name__} type {self}"
-                warnings.warn(message=warn_msg, category=RuntimeWarning, stacklevel=2)
+                with warnings.catch_warnings():
+                    warnings.simplefilter("once")
+                    warn_msg = f"Could not find force constant data for the {type(self).__name__} type {self}"
+                    warnings.warn(message=warn_msg, category=RuntimeWarning, stacklevel=2)
                 # Make an assumption on the force constant
-                _k = 5 * self.order  # Assume 5 newtons per centimetre per order of the bond
+                _k = Quantity(5 * self.order, "N cm^-1")  # Assume 5 newtons per centimetre per order of the bond
 
             self._force_constant = _k
 

src/carpy/physicalchem/_chemical_structure.py
@@ -221,12 +221,17 @@ class PartitionMethods:
         int_e = (self._cv_1d * T) * dof_trans
 
         # Rotation contribution
-        dof_rot = np.isfinite(self.theta_rot.x).sum()
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")  # will get sad for linear molecules where 1 DoF is missing
-            principal_activations = partition_function(Tcharacteristic=self.theta_rot)
-        # Slice the 3D principle activations by degree of freedoms to ignore the np.posinf 2 DoF linear molecules get
-        int_e += (self._cv_1d * T) * np.nansum(principal_activations[:dof_rot], axis=0)
+        try:
+            dof_rot = np.isfinite(self.theta_rot.x).sum()
+        except NotImplementedError:
+            dof_rot = 3
+            int_e += (self._cv_1d * T) * 3  # Assume molecule is so complex that it must have 3 DoF at reasonable temps
+        else:
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")  # will get sad for linear molecules where 1 DoF is missing
+                principal_activations = partition_function(Tcharacteristic=self.theta_rot)
+            # Slice the 3D principle activations by degree of freedoms to ignore the np.posinf 2 DoF linear molecules get
+            int_e += (self._cv_1d * T) * np.nansum(principal_activations[:dof_rot], axis=0)
 
         # Vibration contribution
         dof_vib = 2 * (3 * len(self.atoms) - (dof_trans + dof_rot))
@@ -277,7 +282,7 @@ class PartitionMethods:
             dof_rot = np.isfinite(self.theta_rot.x).sum()
         except NotImplementedError:
             dof_rot = 3
-            cv += self._cv_1d * dof_rot  # Assume molecule is so complicated, it must have three DoF at reasonable temps
+            cv += self._cv_1d * dof_rot  # Assume molecule is so complex that it must have 3 DoF at reasonable temps
         else:
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore")  # will get sad for linear molecules where 1 DoF is missing

src/carpy/physicalchem/_equations_of_state.py
@@ -85,7 +85,7 @@ class EquationOfState:
         # Dong and Lienhard's relation
         numerator = np.log(p_rs) - 5.372_70 * (1 - 1 / T_r)
         denominator = 7.494_08 - 11.181_777 * T_r ** 3 + 3.687_69 * T_r ** 6 + 17.929_98 * np.log(T_r)
-        omega = float(numerator / denominator)
+        omega = (numerator / denominator).item()
         return omega
 
     @omega.setter